02aa8241b89f22c7cc4c33316b71e6c0688e972e
jpgonzalez436 <jpgonzales-2024b@corhuila.edu.co>
2025-10-08 21:55:55 -0500

    fix: :bug: getting correct values from dot env in settings

--- bancowo/settings.py
+++ bancowo/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 from os import getenv
-
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -84,7 +85,7 @@ DATABASES = {
         "NAME": getenv("DATABASE_NAME"),
         "USER": getenv("DATABASE_USER"),
         "PASSWORD": getenv("DATABASE_PASSWORD"),
-        "HOST": getenv("DATABASE_HOST", "db"),
+        "HOST": getenv("DATABASE_HOST"),
         "PORT": getenv("DATABASE_PORT", "5432"),
     }
 }
